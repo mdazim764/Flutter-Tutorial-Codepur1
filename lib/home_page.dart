a740e832fd5263849b1081f4eb862c2d86a547b6
azimk <khairdimdazim@gmail.com>
2024-12-15 12:06:23 +0530

    day2

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget{
+ final int age=21;
+  var name='azim';
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "first app"
+        ),
+      ),
+      body:Center(
+        child: Container(
+          child: Text("name: $name\nage: $age"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
